cb6062e714b416caf99583d70e8211152bcdad32
annabjurback <annabjurback@gmail.com>
2023-02-27 15:09:30 +0100

    javascipt scramble

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-let todoListTemplate = document.querySelector('#todo-template');
-let todoContent = document.querySelector('#todo-content');
-todoContent.appendChild(todoListTemplate.cloneNode(true).content);
 let form = document.querySelector('#input');
 
+form.onsubmit = event => {
+    event.preventDefault();
+    
+    let todoListTemplate = document.querySelector('#todo-template');
+    let todoContent = document.querySelector('#todo-content');
+    todoContent.appendChild(todoListTemplate.cloneNode(true).content);
+    let newItemContent = form.elements.inputField.value;
+    let newListItem = todoListTemplate.ul.li.cloneNode(true).content;
 
+
+};
 
