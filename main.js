06ad001f3b39ee48dbb0a918844433d241acc31c
Max Hansen <meepmopo@gmail.com>
2023-02-27 16:16:54 +0100

    Some styles and item templates

--- main.js
+++ main.js
@@ -3,17 +3,22 @@ let todoListTemplate = document.querySelector('#todo-template');
 let todoContent = document.querySelector('#todo-content');
 
 todoContent.appendChild(todoListTemplate.cloneNode(true).content);
+let list = todoContent.querySelector('ul');
+
+let todoItemTemplate = document.querySelector('#item-template');
 
 form.onsubmit = event => {
     event.preventDefault();
     
     let textInput = form.elements.inputField.value;
-    let list = todoContent.querySelector('ul');
-    todoContent.cloneNode(true); // ???
-    let newItem = list.querySelector('li');
+
+    let newItem = todoItemTemplate.cloneNode(true).content;
+
     let newLabel = newItem.querySelector('label');
     newLabel.textContent = textInput;
     list.appendChild(newItem);
+
+    form.elements.inputField.value = '';
 };
 
  
