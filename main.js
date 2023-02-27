96a5c6784eace86b9f61dd81cc63047707b3de5d
annabjurback <annabjurback@gmail.com>
2023-02-27 15:33:55 +0100

    javascript continued work

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 let form = document.querySelector('#input');
+let todoListTemplate = document.querySelector('#todo-template');
+let todoContent = document.querySelector('#todo-content');
+
+todoContent.appendChild(todoListTemplate.cloneNode(true).content);
 
 form.onsubmit = event => {
     event.preventDefault();
     
-    let todoListTemplate = document.querySelector('#todo-template');
-    let todoContent = document.querySelector('#todo-content');
-    todoContent.appendChild(todoListTemplate.cloneNode(true).content);
-    let newItemContent = form.elements.inputField.value;
-    let newListItem = todoListTemplate.ul.li.cloneNode(true).content;
-
-
+    let textInput = form.elements.inputField.value;
+    let list = todoContent.querySelector('ul');
+    todoContent.cloneNode(true); // ???
+    let newItem = list.querySelector('li');
+    let newLabel = newItem.querySelector('label');
+    newLabel.textContent = textInput;
+    list.appendChild(newItem);
 };
 
+ 
